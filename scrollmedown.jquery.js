093b01787b253d666138991b77ed944a28495663
DanialartBiz <genyo@mail.ru>
2015-09-20 18:59:05 +0300

    v.0.0.1

--- scrollmedown.jquery.js
+++ scrollmedown.jquery.js
@@ -1,16 +1,38 @@
 // test for infiniti scrolling v0.1;
-	function loadNewPosts(container, selector) {
-		$.ajax({
-			type:"GET",
-			url: window.location.host,
-			dataType:"html",
-			success: function(data) {
-				container.remove();
-			}
-		})
-	} //
-	$(window).scroll(function() {
-		if($(window).scrollTop() + $(window).height() >= $(document).height() - 100) {
-			loadNewPosts('.latest .items', '.item');
+jQuery(function($){
+	$(window).scroll(function(){
+		var data = {
+			'action': 'loadmore',
+			'query': true_posts,
+			'page' : current_page
+		};
+
+		var marginBottom = 1800;
+		if( $(document).scrollTop() > ($(document).height() - marginBottom)){
+			$('.loader').remove();
+			$.ajax({
+				url:ajaxurl,
+				data:data,
+				type:'POST',
+				beforeSend: function( xhr){
+					$('body').addClass('loading');
+					if(!$('.loader').length > 0) {
+					site_container.append('<div class="loader"><i class="icon">!</i></div>');
+					$('.loader').addClass('loading').show();
+					}
+
+				},
+				success:function(data){
+					if( data ) { 
+						items_container.delay(200).append(data);
+						current_page++;
+					}
+				},
+				complete:function() {
+					$('.loader').hide();
+				}
+			});
 		}
-	})
+	});
+
+});
